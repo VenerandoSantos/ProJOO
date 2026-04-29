33ae420d233ef15e4d61c90e312a9ca37acf8609
VenerandoSantos <162456400+VenerandoSantos@users.noreply.github.com>
2026-04-28 22:28:42 -0300

    Add Visitor pattern for file conversion classes
    Implemented Visitor pattern for file conversion with HTML support.

--- src/ProjetoVisitor/main.ts
+++ src/ProjetoVisitor/main.ts
@@ -1,1 +1,59 @@
+/* 
+# 6 classes de arquivos Diferentes
+- html
+- exel
+- doc
+- word
+- powerBI
+- pdf
 
+cada um podera gerar um outro tipo de arquivo
+gerarPDF()
+gerarHTML()
+
+
+*/
+
+interface Visitor {
+    visit(arq: Html) :void ;
+}
+
+abstract class Arquivos {
+    private texto :string;
+
+    constructor (texto:string){
+        this.texto = texto;
+    }
+
+    get getTexto (){
+        return this.texto
+    }
+
+    aceept (visita: Visitor){}
+}
+
+class Html extends Arquivos {
+
+    impressao () {
+        console.log ("oi, eu sou um HTML.\n");
+    }
+
+    accept (visitor:Visitor){
+        visitor.visit(this);
+    }
+}
+
+
+/* Classe Visitante */
+class ConvertArquivoPDFVisitor implements Visitor {
+    public visit (arq: Html){
+        console.log ("arquivo HTML Convertido para PDF")
+    }
+
+}
+
+function teste () {
+    console.log ("hello world")
+}
+
+teste ();
